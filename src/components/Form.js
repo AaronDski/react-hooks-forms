a4e4f7ae41563a3c0ccd4623f91824704c9916ff
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-25 17:29:05 -0600

    commit tree 00f2a81f1eae8b2f6a0d3d95452551588a55c5ab

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form(props) {
 
-  return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+
+
+  // function Form(props) {
+    return (
+      <form>
+        <input
+          type="text"
+          onChange={props.handleFirstNameChange}
+          value={props.firstName}
+        />
+        <input
+          type="text"
+          onChange={props.handleLastNameChange}
+          value={props.lastName}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  // const [newsetter, setNewsetter] = useState(false);
+
+  // function handleNewsletterChange(event) {
+  //   // .checked, not .value!
+  //   setNewsetter(event.target.checked);
+  // }
+
+  // return (
+  //   <form>
+  //     <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+  //     <input
+  //       type="checkbox"
+  //       id="newsletter"
+  //       onChange={handleNewsletterChange}
+  //       checked={newsetter}
+  //     />
+  //     <button type="submit">Submit</button>
+  //   </form>
+  // );
+
+
+
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+
+  // function handleFirstNameChange(event) {
+  //   setFirstName(event.target.value);
+  // }
+  
+  // function handleLastNameChange(event) {
+  //   setLastName(event.target.value);
+  // } 
+
+  // return (
+  //   <form>
+  //     <input type="text" onChange={handleFirstNameChange} value={firstName} />
+  //     <input type="text" onChange={handleLastNameChange} value={lastName} /> 
+  //     <button type="submit">Submit</button>
+  //   </form>
+  // );
 }
 
 export default Form;
